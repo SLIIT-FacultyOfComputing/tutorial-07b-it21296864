ee638b231916313b0aa527d4a4aadb47f9e7db72
it21296864 <105943293+it21296864@users.noreply.github.com>
2022-05-22 11:06:03 +0000

    Tutorial 07 answer

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -15,14 +15,29 @@ int main() {
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
    // 1. Create a dynamic Rectangle type variable (pointer)
-   // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
-   // 3. Create a dynamic Circle type variable (pointer)
-   // 4. Create a dynamic Circle Object set radius that was input from the keyboard
-   // 5. Call the display method of the Rectangle Object
-   // 6. Call the display method of the Circle Object
-   // 7. Delete the Rectangle Object from memory
-   // 8. Delete the Circle Object from memory
   
+   // 2. Create a dynamic Rectangle Object set the length and width that was input from the keyboard
+  Rectangle *r;
+  Rectangle *r1;
+  r=new Rectangle();
+  r1=new Rectangle(length,width);
+
+  
+   // 3. Create a dynamic Circle type variable (pointer)
+  
+   // 4. Create a dynamic Circle Object set radius that was input from the keyboard
+  Circle *c;
+  Circle *c1;
+  c= new Circle();
+  c1= new Circle(radius);
+   // 5. Call the display method of the Rectangle Object
+  r1->display();
+   // 6. Call the display method of the Circle Object
+  c1->display();
+   // 7. Delete the Rectangle Object from memory
+  delete r1;
+   // 8. Delete the Circle Object from memory
+  delete c1;
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
    cout << "End of Program" << endl;
--- Item.cpp
+++ Item.cpp
@@ -5,6 +5,17 @@ using namespace std;
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
+Item::Item(){
+  itemCode=0;
+  unitPrice=0;
+}
+Item::Item(int iCode,float unitprice){
+  itemCode=iCode;
+  unitPrice=unitprice;
+}
+Item::~Item(){
+  cout<<"Destructor called"<<endl;
+}
 
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
